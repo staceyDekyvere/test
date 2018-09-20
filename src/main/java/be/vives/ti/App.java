1d70303d5cc02fce1ffc0c9b2f09008529bb0406
Stacey Dekyvere <stacey.dekyvere@student.vives.be>
2018-09-20 18:03:18 +0200

    dit is een test

---
+++ src/main/java/be/vives/ti/App.java
@@ -0,0 +1,13 @@
+package be.vives.ti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
